12c94395cf1fbdd6c6c195fe50136582e6534da7
nvila-iso <nvila88@proton.me>
2025-09-17 19:58:57 -0400

    added recent activities data display template

--- src/components/Profile/OverviewOptions/RecentActivities.jsx
+++ src/components/Profile/OverviewOptions/RecentActivities.jsx
@@ -1,7 +1,15 @@
 const RecentActivities = () => {
   return (
     <>
-      <div className="flex flex-col mt-5">
+      <div className="flex flex-col mt-5 gap-5">
+        <div className="w-full h-24 border border-zinc-300 rounded-lg p-5 ">
+          <div className="flex justify-between">
+            <p>Listed new product</p>
+            <p className="text-zinc-500">2 hours ago</p>
+          </div>
+          <p className="text-zinc-500">MacBookPro</p>
+        </div>
+
         <div className="w-full h-24 border border-zinc-300 rounded-lg p-5">
           <div className="flex justify-between">
             <p>Listed new product</p>
