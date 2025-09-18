68f7e7932e5e3a13023a058e5787926c440ad2d6
nvila-iso <nvila88@proton.me>
2025-09-17 21:07:00 -0400

    settings template completed. data population set for tomorrow

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
             <select
               name="state"
               className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-state"
+              id="state"
             >
               <option value="" selected disabled hidden>
                 🇺🇸 Select you state
