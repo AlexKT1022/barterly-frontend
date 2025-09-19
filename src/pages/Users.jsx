f4594f4e6f1245f6141e23900ac92f6c4e45fbe1
nvila-iso <nvila88@proton.me>
2025-09-18 20:58:13 -0400

    created users page / added to app.jsx

---
+++ src/pages/Users.jsx
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <>
+      <div className="user-profile mx-auto mt-5 h-42 border rounded-lg border-zinc-300 grid grid-cols-[100px_minmax(900px,_1fr)_100px] gap-5 p-5"></div>
+    </>
+  );
+};
+
+export default Users;
