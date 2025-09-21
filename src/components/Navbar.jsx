f4dca7c1ae832bea4345c0faae4fdb093f9c3321
nvila-iso <nvila88@proton.me>
2025-09-21 01:10:08 -0400

    took a while but i get it now. nav responsivness tested / works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,50 +5,51 @@ const Navbar = () => {
   const { token, logout } = useAuth();
   return (
     <>
-      <div className="flex justify-between items-center h-12">
-        <div className="font-bold flex items-center text-lg">
-          <img src="/bartley_outline.svg" className="mr-1 h-6 w-auto" />
-          <NavLink to="/"> Barterly </NavLink>
-        </div>
-        <div className="flex gap-5 text-lg">
-          <NavLink to="/explore" className="hover-underline-animation">
-            Explore
-          </NavLink>
-          <NavLink to="/categories" className="hover-underline-animation">
-            Categories
-          </NavLink>
-          <NavLink to="/how-it-works" className="hover-underline-animation">
-            How It Works
-          </NavLink>
-          <NavLink to="/about" className="hover-underline-animation">
-            About
-          </NavLink>
-        </div>
-        <div className="flex gap-5"></div>
+      <nav className="">
+        <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
+          <div className="font-bold flex items-center justify-center lg:justify-normal text-lg">
+            <img src="/bartley_outline.svg" className="mr-1 h-6 w-auto" />
+            <NavLink to="/"> Barterly </NavLink>
+          </div>
+          <div className="flex w-full justify-center gap-5 lg:text-lg md:text-md md:auto">
+            <NavLink to="/explore" className="hover-underline-animation">
+              Explore
+            </NavLink>
+            <NavLink to="/categories" className="hover-underline-animation">
+              Categories
+            </NavLink>
+            <NavLink to="/how-it-works" className="hover-underline-animation">
+              How It Works
+            </NavLink>
+            <NavLink to="/about" className="hover-underline-animation">
+              About
+            </NavLink>
+          </div>
+          <div className="flex gap-5"></div>
 
-        <div className="flex gap-5">
-          {token ? (
-            <>
-              <NavLink to="/profile">Profile</NavLink>
-              <NavLink onClick={logout}>Log Out</NavLink>
-            </>
-          ) : (
-            <>
-              <NavLink to="/register">
-                <button className="px-4 h-10 rounded-md bg-zinc-800 text-white transition-colors duration-300 hover:bg-zinc-500 shadow-sm">
-                  Sign Up
-                </button>
-              </NavLink>
-              <NavLink to="/login">
-                <button className="px-4 h-10 rounded-md border-1 border-zinc-500 transition-colors duration-300 hover:bg-zinc-500 hover:text-white hover:border-transparent shadow-sm">
-                  Login
-                </button>
-              </NavLink>
-              
-            </>
-          )}
+          <div className="flex gap-5 justify-center">
+            {token ? (
+              <>
+                <NavLink to="/profile">Profile</NavLink>
+                <NavLink onClick={logout}>Log Out</NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink to="/register">
+                  <button className="px-4 w-24 h-10 rounded-md bg-zinc-800 text-white transition-colors duration-300 hover:bg-zinc-500 shadow-sm">
+                    Sign Up
+                  </button>
+                </NavLink>
+                <NavLink to="/login">
+                  <button className="px-4 w-24 h-10 rounded-md border-1 border-zinc-500 transition-colors duration-300 hover:bg-zinc-500 hover:text-white hover:border-transparent shadow-sm">
+                    Login
+                  </button>
+                </NavLink>
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      </nav>
     </>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,74 +1,76 @@
-import { Link } from 'react-router';
+import { Link } from "react-router";
 
 const Homepage = () => {
   return (
     <>
-      <div className='flex flex-col justify-center items-center mx-auto h-200'>
-        <h1 className='font-bold w-lg text-center text-base/15 mb-5'>
-          Trade goods and services directly
-        </h1>
-        <h2 className='text-center w-xl mb-5'>
-          Barterly is a platform for swapping items and skills without the use
-          of money. Start trading today!
-        </h2>
-        <Link to=''>
-          <button className='px-4 h-10 rounded-md bg-zinc-800 text-white transition-colors duration-300 hover:bg-zinc-500 shadow-sm mb-5'>
-            Get Started
-          </button>
-        </Link>
-        <div className='mb-10'>
-          <form>
-            <input
-              className='w-lg h-10 rounded-md border-1 border-zinc-400 mr-3'
-              placeholder='Search for items or services...'
-              id='home-search'
-            ></input>
-            <Link to=''>
-              <button className='px-8 h-10 rounded-md bg-zinc-800 text-white transition-colors duration-300 hover:bg-zinc-500 shadow-sm'>
-                Search
-              </button>
+      <section className="">
+        <div className="flex flex-col justify-center items-center h-200">
+          <h1 className="font-bold w-lg text-center text-base/15 mb-5">
+            Trade goods and services directly
+          </h1>
+          <h2 className="text-center w-xl mb-5">
+            Barterly is a platform for swapping items and skills without the use
+            of money. Start trading today!
+          </h2>
+          <Link to="">
+            <button className="px-4 h-10 rounded-md bg-zinc-800 text-white hover:bg-zinc-500 transition-colors duration-300  shadow-sm mb-5">
+              Get Started
+            </button>
+          </Link>
+          <div className="mb-10">
+            <form>
+              <input
+                className="w-lg h-10 rounded-md border-1 border-zinc-400 mr-3"
+                placeholder="Search for items or services..."
+                id="home-search"
+              ></input>
+              <Link to="">
+                <button className="px-8 h-10 rounded-md bg-zinc-800 text-white transition-colors duration-300 hover:bg-zinc-500 shadow-sm">
+                  Search
+                </button>
+              </Link>
+            </form>
+          </div>
+          <div className="grid xs:grid-cols-2 grid-cols-4 gap-15">
+            <Link
+              to=""
+              className="p-5 shadow-md hover:shadow-xl transition-all duration-300"
+            >
+              <div className="flex flex-col justify-center items-center">
+                <img src="src\assets\electronics.png"></img>
+                <p>Electronics</p>
+              </div>
             </Link>
-          </form>
+            <Link
+              to=""
+              className="p-5 shadow-md hover:shadow-xl transition-all duration-300"
+            >
+              <div className="flex flex-col justify-center items-center">
+                <img src="src\assets\clothing.png"></img>
+                <p>Clothing</p>
+              </div>
+            </Link>
+            <Link
+              to=""
+              className="p-5 shadow-md hover:shadow-xl transition-all duration-300"
+            >
+              <div className="flex flex-col justify-center items-center">
+                <img src="src\assets\automotive.png"></img>
+                <p>Automotive</p>
+              </div>
+            </Link>
+            <Link
+              to=""
+              className="p-5 shadow-md hover:shadow-xl transition-all duration-300"
+            >
+              <div className="flex flex-col justify-center items-center">
+                <img src="src\assets\services.png"></img>
+                <p>Services</p>
+              </div>
+            </Link>
+          </div>
         </div>
-        <div className='grid grid-cols-4 gap-15'>
-          <Link
-            to=''
-            className='p-5 shadow-md hover:shadow-xl transition-all duration-300'
-          >
-            <div className='flex flex-col justify-center items-center'>
-              <img src='src\assets\electronics.png'></img>
-              <p>Electronics</p>
-            </div>
-          </Link>
-          <Link
-            to=''
-            className='p-5 shadow-md hover:shadow-xl transition-all duration-300'
-          >
-            <div className='flex flex-col justify-center items-center'>
-              <img src='src\assets\clothing.png'></img>
-              <p>Clothing</p>
-            </div>
-          </Link>
-          <Link
-            to=''
-            className='p-5 shadow-md hover:shadow-xl transition-all duration-300'
-          >
-            <div className='flex flex-col justify-center items-center'>
-              <img src='src\assets\automotive.png'></img>
-              <p>Automotive</p>
-            </div>
-          </Link>
-          <Link
-            to=''
-            className='p-5 shadow-md hover:shadow-xl transition-all duration-300'
-          >
-            <div className='flex flex-col justify-center items-center'>
-              <img src='src\assets\services.png'></img>
-              <p>Services</p>
-            </div>
-          </Link>
-        </div>
-      </div>
+      </section>
     </>
   );
 };
