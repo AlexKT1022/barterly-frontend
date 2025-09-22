1551188b9746c6d175ac12e5fcaa9792294a5cc2
AlexKT1022 <alexkt1022@gmail.com>
2025-09-22 08:45:48 -0400

    improved profile loader

--- src/loaders/profileLoader.js
+++ src/loaders/profileLoader.js
@@ -1,6 +1,9 @@
 const profileLoader = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/users/me'); // refactor as needed
+    const token = sessionStorage.getItem('token');
+    const res = await fetch('http://localhost:3000/api/users/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     const data = await res.json();
 
     console.log(data);
