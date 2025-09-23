4e45a5b62156a47a88518431b7481c87e8dfafda
nvila-iso <nvila88@proton.me>
2025-09-23 11:44:13 -0400

    images now load

--- src/components/Categories/ProductCard.jsx
+++ src/components/Categories/ProductCard.jsx
@@ -1,13 +1,47 @@
+import { useEffect, useState } from "react";
+
+const fetchItemsByPostId = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/posts/${id}`);
+    return await res.json();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const ProductCard = ({ post }) => {
+  const [postData, setPostData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const loadData = async () => {
+      const data = await fetchItemsByPostId(post.id);
+      console.log(data);
+      setPostData(data);
+    };
+    try {
+      loadData();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
   const status = {
     open: "bg-green-100 text-green-600",
     pending: "bg-yellow-100 text-yellow-600",
     closed: "bg-red-100 text-red-600",
   };
+
+  if (loading) return <p>loading</p>;
+
   return (
     <div className="flex flex-col rounded-lg border border-zinc-300 w-80 min-h-80 p-5 hover:shadow-md transition-shadow">
       <img
-        src=""
+        src={postData.items?.[0].imageUrl}
         alt=""
         className="w-full bg-black rounded-lg mb-1 h-40 object-cover object-top"
       />
